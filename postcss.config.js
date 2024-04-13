8bfd162b6238083d92955b1e0005499a467ccb1e
Felipe Alarcon <felipe.alarcon.rozo@gmail.com>
2024-04-13 13:17:05 +0200

    fix safari css nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+	plugins: {
+		'tailwindcss/nesting': 'postcss-nesting',
+		tailwindcss: {},
+		'postcss-preset-env': {
+			browsers: ['last 3 version'],
+			features: {
+				'nesting-rules': false,
+				'custom-selectors': { preserve: false },
+			},
+		},
+	},
+}
