d9266113af1058c614cdc0c20e3b4562cdc10215
Felipe Alarcon <felipe.alarcon.rozo@gmail.com>
2024-05-22 21:49:54 +0200

    chore: Refactor HomePagePreview component

--- src/components/pages/home/HomePagePreview.tsx
+++ src/components/pages/home/HomePagePreview.tsx
@@ -10,23 +10,25 @@ import HomePage from './HomePage'
 import { i18n } from '../../../../i18n-config'
 
 type Props = {
-    initial: QueryResponseInitial<HomePagePayload | null>
+	initial: QueryResponseInitial<HomePagePayload | null>
 }
 
 export default function HomePagePreview(props: Props) {
-    console.log({ props })
-    const { initial } = props
-    const { data, encodeDataAttribute } = useQuery<HomePagePayload | null>(
-        homePageQuery,
-        {
-            language: initial?.data?.language || i18n.defaultLocale,
-        },
-        { initial }
-    )
+	console.log({ props })
+	const { initial } = props
+	const { data, encodeDataAttribute } = useQuery<HomePagePayload | null>(
+		homePageQuery,
+		{
+			language: initial?.data?.language || i18n.base,
+		},
+		{ initial }
+	)
 
-    if (!data) {
-        return <div className="text-center">Please start editing your Home document to see the preview!</div>
-    }
+	if (!data) {
+		return (
+			<div className="text-center">Please start editing your Home document to see the preview!</div>
+		)
+	}
 
-    return <HomePage data={data} encodeDataAttribute={encodeDataAttribute} />
+	return <HomePage data={data} encodeDataAttribute={encodeDataAttribute} />
 }
